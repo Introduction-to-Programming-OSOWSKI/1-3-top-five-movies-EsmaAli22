32beb7c5d4011f9b4112027050801fac6277d84d
EsmaAli22 <824194@mystma.org>
2021-03-25 03:58:59 +0000

    topFiveMovies try 11

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies(a, b, c, d, e):
-    return a + b + c + d + "and " + e
+    return a + b + c + e
 
-print (topFiveMovies("Annabelle ","Conjuring ","It ","Avengers:End-Game ","Conjuring 2"))
+print (topFiveMovies("Annabelle", "Conjuring", "It", "Avengers:End-Game", "Conjuring 2"))
